3bef567afc407852383e3030e2ade0d4a5d99804
Iswenzz <suxlolz1528@gmail.com>
2020-12-31 10:59:18 +0100

    Compile scripts + SR.md

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-# compile
+echo -e "[+] Compile\n"
 make
 
-# copy binary
-cp bin/cod4x18_dedrun.exe "D:\Program Files (x86)\Activision\Cod4Mod\cod4x18_dedrun.exe"
+echo -e "[+] Copy Binary\n"
+cp bin/cod4x18_dedrun.exe /home/cod4/cod4x18_dedrun.exe
