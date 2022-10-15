3d3c421dc6c13f611d30e47b1dc976a52cd8c48c
Iswenzz <suxlolz1528@gmail.com>
2022-10-15 18:09:48 +0200

    Demo: Spastic linkage not working

--- src/SR/game/Demo.cpp
+++ src/SR/game/Demo.cpp
@@ -5,15 +5,11 @@ namespace Iswenzz::CoD4x
 	Demo::Demo()
 	{
 		std::string path = R"(D:\Vids\CoD4\izengine\mp_dr_darmuhv2.dm_1)";
-		// Reader = std::make_unique<Iswenzz::CoD4::DM1::DemoReader>(path);
-		// auto a = Reader->IsOpen();
-
-		SV_BotAllocateClient();
 	}
 
 	Demo::~Demo()
 	{
-		// Reader->Close();
+
 	}
 
 	void Demo::PlayDemo()
